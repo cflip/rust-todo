8b9ef713dffc0ae39b5f5311c5a2350c1877b879
Cflip <36554078+cflip@users.noreply.github.com>
2020-09-12 11:54:49 -0600

    Add basic argument parsing with clap

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
-use std::env;
+use clap::{load_yaml, App};
 
 fn main() {
-	let args: Vec<String> = env::args().collect();
-	args.iter().for_each(|argument| {
-		println!("{}", argument);
+	let args_yaml = load_yaml!("../args.yml");
+	let matches = App::from_yaml(args_yaml).get_matches();
+
+	let file_path = matches.value_of("file").unwrap();
+	println!("Will be reading from: \n{}", file_path);
+
+	matches.value_of("view").and_then(|index| -> Option<&str> {
+		println!("Viewing index #{}", index);
+		Some(index)
+	});
+
+	matches.value_of("edit").and_then(|index| -> Option<&str> {
+		println!("Editing index #{}", index);
+		Some(index)
 	});
 }
