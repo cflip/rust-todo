1caa7c12f896d8eefcbfc8c148ca5c5f34eba994
h5p9sl <21267024+h5p9sl@users.noreply.github.com>
2020-09-12 17:00:17 -0600

    Remove example comments

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,6 @@ fn main() {
     let file_path = matches.value_of("file").unwrap().to_string();
     let todo_list = todo::TodoList::from_file(&file_path).unwrap();
 
-    // Example code that you could implement:
-    //      todo_list.get_item("laundry").uncheck();
-    //      todo_list.get_item("code rust").check();
-    //      todo_list.get_item("foo").rename("oof");
-    //      todo_list.remove_item("foo");
-    //      todo_list.add_item(TodoItem::new("watch movie", None, false));
-
     matches.value_of("view").and_then(|index| -> Option<&str> {
         println!("Viewing index #{}", index);
         Some(index)
