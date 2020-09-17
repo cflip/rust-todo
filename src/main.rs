c57f15825a810cb694d48f3b78189874f2652727
Cflip <36554078+cflip@users.noreply.github.com>
2020-09-17 16:26:36 -0600

    Configure rustfmt to indent using tabs

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,22 @@ use clap::{load_yaml, App};
 mod todo;
 
 fn main() {
-    let args_yaml = load_yaml!("../args.yml");
-    let matches = App::from_yaml(args_yaml).get_matches();
+	let args_yaml = load_yaml!("../args.yml");
+	let matches = App::from_yaml(args_yaml).get_matches();
 
-    let file_path = matches.value_of("file").unwrap().to_string();
-    let todo_list = todo::TodoList::from_file(&file_path).unwrap();
-    for item in todo_list.as_vec() {
-        println!("{:?}", item);
-    }
+	let file_path = matches.value_of("file").unwrap().to_string();
+	let todo_list = todo::TodoList::from_file(&file_path).unwrap();
+	for item in todo_list.as_vec() {
+		println!("{:?}", item);
+	}
 
-    matches.value_of("view").and_then(|index| -> Option<&str> {
-        println!("Viewing index #{}", index);
-        Some(index)
-    });
+	matches.value_of("view").and_then(|index| -> Option<&str> {
+		println!("Viewing index #{}", index);
+		Some(index)
+	});
 
-    matches.value_of("edit").and_then(|index| -> Option<&str> {
-        println!("Editing index #{}", index);
-        Some(index)
-    });
+	matches.value_of("edit").and_then(|index| -> Option<&str> {
+		println!("Editing index #{}", index);
+		Some(index)
+	});
 }
--- src/todo.rs
+++ src/todo.rs
@@ -2,70 +2,70 @@ use std::{fs, path::PathBuf};
 
 #[derive(Debug, Clone)]
 pub enum TodoListError {
-    InvalidFormat(String),
+	InvalidFormat(String),
 }
 
 #[derive(Debug, Clone)]
 pub struct TodoItem {
-    name: String,
-    desc: String,
-    complete: bool,
+	name: String,
+	desc: String,
+	complete: bool,
 }
 
 pub struct TodoList {
-    items: Vec<TodoItem>,
+	items: Vec<TodoItem>,
 }
 
 impl TodoItem {
-    pub fn new<S: Into<String>>(name: S, desc: S, complete: bool) -> TodoItem {
-        let (name, desc, complete) = (name.into(), desc.into(), complete);
-        TodoItem {
-            name,
-            desc,
-            complete,
-        }
-    }
+	pub fn new<S: Into<String>>(name: S, desc: S, complete: bool) -> TodoItem {
+		let (name, desc, complete) = (name.into(), desc.into(), complete);
+		TodoItem {
+			name,
+			desc,
+			complete,
+		}
+	}
 }
 
 impl Default for TodoList {
-    fn default() -> TodoList {
-        TodoList { items: Vec::new() }
-    }
+	fn default() -> TodoList {
+		TodoList { items: Vec::new() }
+	}
 }
 
 impl TodoList {
-    pub fn from_string<S: Into<String>>(string: S) -> Result<TodoList, TodoListError> {
-        let mut result = Self::default();
+	pub fn from_string<S: Into<String>>(string: S) -> Result<TodoList, TodoListError> {
+		let mut result = Self::default();
 
-        for line in string.into().lines() {
-            let split_line: Vec<&str> = line.split(';').collect();
+		for line in string.into().lines() {
+			let split_line: Vec<&str> = line.split(';').collect();
 
-            if split_line.len() < 3 {
-                return Err(TodoListError::InvalidFormat(
-                    "File format error: Not enough information in file".to_string(),
-                ));
-            }
+			if split_line.len() < 3 {
+				return Err(TodoListError::InvalidFormat(
+					"File format error: Not enough information in file".to_string(),
+				));
+			}
 
-            let is_complete: bool = split_line.get(0).unwrap().parse().unwrap();
-            let title: String = String::from(*split_line.get(1).unwrap_or(&"Untitled"));
-            let desc: String = String::from(*split_line.get(2).unwrap_or(&""));
+			let is_complete: bool = split_line.get(0).unwrap().parse().unwrap();
+			let title: String = String::from(*split_line.get(1).unwrap_or(&"Untitled"));
+			let desc: String = String::from(*split_line.get(2).unwrap_or(&""));
 
-            result.items.push(TodoItem::new(title, desc, is_complete));
-        }
-        Ok(result)
-    }
+			result.items.push(TodoItem::new(title, desc, is_complete));
+		}
+		Ok(result)
+	}
 
-    pub fn from_file<P: Into<PathBuf>>(path: P) -> Result<TodoList, TodoListError> {
-        let path: PathBuf = path.into();
-        let file_contents = fs::read_to_string(path.as_path()).unwrap();
-        Self::from_string(file_contents)
-    }
+	pub fn from_file<P: Into<PathBuf>>(path: P) -> Result<TodoList, TodoListError> {
+		let path: PathBuf = path.into();
+		let file_contents = fs::read_to_string(path.as_path()).unwrap();
+		Self::from_string(file_contents)
+	}
 
-    pub fn as_vec(&self) -> &Vec<TodoItem> {
-        &self.items
-    }
+	pub fn as_vec(&self) -> &Vec<TodoItem> {
+		&self.items
+	}
 
-    pub fn as_mut_vec(&mut self) -> &mut Vec<TodoItem> {
-        &mut self.items
-    }
+	pub fn as_mut_vec(&mut self) -> &mut Vec<TodoItem> {
+		&mut self.items
+	}
 }
